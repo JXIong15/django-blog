ea2ba68949825a79d163c84018e738d8917cd559
Jou Xiong <jouxiong15@gmail.com>
2021-09-18 21:41:08 -0500

    added individual post view

--- post/views.py
+++ post/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'post/index.html')
+    posts = Post.objects.all()
+    return render(request, 'post/index.html', {'posts': posts})
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'post/post.html', {'post': post})
